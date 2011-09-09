bbcfe26640269127cd3972e4dd86a61be133c116
Eric Xing <ericyanzhe@gmail.com>
2011-09-09 18:15:04 +0900

    finish apache_log_parser_split.py

--- python_for_unix_and_linux_system_administration/ch3/apache_log_parser_split.py
+++ python_for_unix_and_linux_system_administration/ch3/apache_log_parser_split.py
@@ -23,3 +23,36 @@ def dictify_logline(line):
             'bytes_sent': split_line[9],
             }
 
+def generate_log_report(logfile):
+    '''return a dictionary of format remote_host=>[list of bytes sent]
+
+    This function takes a file object, iterates through all the lines in the file,
+    and generates a report of the number of bytes transferred to each remote host
+    for each hit on the webserver.
+    '''
+    report_dic = {}
+    for line in logfile:
+        line_dict = dictify_logfile(line)
+        print line_dict
+        try:
+            bytes_sent = int(line_dict['bytes_sent'])
+        except ValueError:
+            ##totally disregard anything we don't understand
+            continue
+        report_dict.setdefault(line_dict['remote_host'],[]).append(bytes_sent)
+    return report_dict
+
+if __name__ == "__main__":
+    if not len(sys.argv) > 1:
+        print __doc__
+        sys.exit(1)
+    infile_name = sys.argv[1]
+    try:
+        infile = open(infile_name, 'r')
+    except IOError:
+        print "You must specify a valid file to parse"
+        print __doc__
+        sys.exit(1)
+    log_report = generate_log_report(infile)
+    print log_report
+    infile.close()
